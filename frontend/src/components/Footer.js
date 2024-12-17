43af63b101d559c573781734c6ad309473dacce2
Sabari1212 <146103181+Sabari1212@users.noreply.github.com>
2024-12-17 21:01:24 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -9,21 +9,19 @@ const Footer = () => {
     <div className=' md:flex  p-5 bg-black  py-5  '>
         <div className='md:w-1/3 text-white font-bold '>
             <ul className=' text-center md:text-start'>
-                <li>M.Saravanan, M.B.A.,LL.B</li>
+            <li>M.Saravanan, M.B.A.,LL.B</li>
                 <li>D.MahendraVarman,B.E.,LL.B</li>
                 <li>M.Manoj,B.C.A.,LL.B</li>
                 <li>B.Sree Preethi, B.Sc., LL.B</li>
                 <li>D.K.Bharathkumar , B.Sc.,LL.B</li>
                  <li>V.Vignesh Kumar , B.Sc.,LL.B</li>
                  <li>T.Thasitha, B.Sc.,LL.B</li>
-                <li>R.Ranjitha,B.A.,LL.B</li>
                  <li>K.Gowziya Begam, B.Sc., LL.B</li>
-               
             </ul>
 
         </div>
         <div className='md:w-1/3 pt-[60px] md:pt-[100px] text-white font-bold med'>
-           <h1 > &copy; 2024 TRK ADVOCATE'S. ALL RIGHT RESERVED</h1>
+           <h1> &copy; 2024 TRK ADVOCATE'S. ALL RIGHT RESERVED</h1>
            <ul className='flex justify-center gap-2 '>
             <li className='text-rose-600'><a href='https://www.instagram.com/mahi_tvk_prathap_7?igsh=cW00cGMxdTl6ZjM2s'><FaSquareInstagram/></a></li>
             <li className='text-gray-600'><a href=''><FaSquareXTwitter/></a></li>
@@ -32,9 +30,10 @@ const Footer = () => {
            </ul>
 
         </div>
-        <div className='md:w-1/3 text-white font-bold mt-10 text-center md:pl-48'>
+        <div className='md:w-1/3 text-white font-bold mt-2 text-center md:pl-48'>
+        <h1 className='  font-serif text-red-500 text-xl pb-3'>Address For Service:</h1>
             <p>
-                D.MahendraVarman,B.E.,LL.B.,PRESIDENT<br></br>
+              D.MahendraVarman,B.E.,LL.B.,PRESIDENT<br></br>
                 1/40,Vettavalam Main Road,<br></br>
                 T.Devanur(PT), Kandachipuram(TK), Villupuram(DT)<br></br>
                 CELL:8012374744/8012243711, Pincode-605752. 
