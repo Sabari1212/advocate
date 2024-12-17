a18707c0fcfbebd008157bd98fa1a7fef45b1ce6
Sabari1212 <146103181+Sabari1212@users.noreply.github.com>
2024-12-17 17:44:53 +0530

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -46,11 +46,11 @@ const Header = () => {
     <header className='flex flex-row justify-between md:px-5 bg-black text-white py-5'>
         <div className='flex flex-row  '>
             <img className='h-[100px] w-[100px]' src={logo}></img>
-            <h1 className='text-3xl md:text-4xl font-bold mt-4 md:mt-7  '>TRK ADVOCATE'S</h1>
+            <h1 className='text-3xl md:text-4xl font-bold mt-4 md:mt-7  '><span className='text-red-500'>TRK</span> ADVOCATE'S</h1>
 
         </div>
         <div className=' font-bold text-xl md:ml-[500px] '>
-            <button onClick={()=>setLogin(!login)} className='mt-8  bg-green-600 md:h-[45px] md:w-[85px]  rounded-md hover:bg-blue-600 hover:scale-110 cursor-pointer hover:duration-500'>ADMIN</button>
+            <button onClick={()=>setLogin(!login)} className='mt-8  bg-green-500 md:h-[45px] md:w-[85px]  rounded-md hover:bg-red-500 hover:scale-110 cursor-pointer hover:duration-500'>ADMIN</button>
         </div>
         <div className='hidden md:block head'>
             <ul className='flex gap-5 mt-8 font-bold text-xl '>
@@ -63,8 +63,8 @@ const Header = () => {
          
 
         </div>
-        <button className='block md:hidden  font-bold text-4xl mb-3' onClick={()=>setMenu(!menu)}><IoMenu/></button>
-         {menu && <nav className=' block md:hidden fixed top-[140px] left-0 bg-gray-300 pt-3 w-full head'>
+        <button className='block md:hidden  font-bold text-4xl mb-3 text-red-500' onClick={()=>setMenu(!menu)}><IoMenu/></button>
+         {menu && <nav className=' block md:hidden fixed top-[140px] left-0 bg-gray-800 pt-3 w-full head'>
             <ul onClick={()=>setMenu(!menu)} className='flex flex-col gap-2 font-bold menu'>
                 <li><a href='#home'>Home</a></li>
                 <li><a href='#about'>About Us</a></li>
@@ -78,7 +78,7 @@ const Header = () => {
         </nav>
         
 }
-  {login && <div className='fixed left-[48px] md:left-[600px] top-[300px] md:top-[300px] text-black '>
+  {login && <div className='fixed left-[40px] md:left-[600px] top-[300px] md:top-[300px] text-black '>
             <form onSubmit={handleSubmit} className='flex flex-col bg-slate-800 p-10 md:w-[400px] gap-3'>
                 <h1 className='text-white text-2xl font-bold'>Admin Login</h1>
                 <label  className='text-left text-white'>UserName</label>
@@ -86,7 +86,7 @@ const Header = () => {
                 <label className='text-left text-white'>PassWord</label>
                 <input type='password' name='password' value={password} onChange={(e) => setPw(e.target.value)}></input>
                 
-                <button className='bg-blue-500 w-[100px] ml-[55px] md:ml-[100px] mt-5 rounded-md hover:bg-green-600 hover:scale-110 duration-500' type='submit'>Login</button>
+                <button className='bg-blue-500 w-[100px] ml-[40px] md:ml-[100px] mt-5 rounded-md hover:bg-green-600 hover:scale-110 duration-500' type='submit'>Login</button>
 
                 
                
